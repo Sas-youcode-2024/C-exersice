01730fd2f1288cdad74732fc0610641ef5fdd267
ZAKARIA-MOUFID <zakariamoufid766@gmail.com>
2024-08-07 20:55:47 +0100

    exercise 1

---
+++ ex1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main() {
+   char nom[20], prenom[20], sex ;
+   int age , numero ;
+
+   printf("entrez votre nom ");
+   scanf("%s",&nom);
+   printf("entrez votre prenom ");
+   scanf("%s",&prenom);
+   printf("entrez votre sex ");
+   scanf(" %c",&sex);
+   printf("entrez votre age ");
+   scanf("%d",&age);
+   printf("entrez votre numero ");
+   scanf("%d",&numero);
+
+
+   printf("vos informations personnelles \n nom : %s \n prenome: %s \n sexe : %c \n age : %d \n numero de thelephone : %d",nom,prenom,sex,age,numero);
+
+
+   return 0 ;
+}
