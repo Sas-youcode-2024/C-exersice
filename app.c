572954f14e45eab7796d295707c12255294c7029
ZAKARIA-MOUFID <zakariamoufid766@gmail.com>
2024-08-07 19:04:39 +0100

    add challenge 2,

---
+++ app.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+   int C ;
+   float F ;
+   printf("veuillez saiser la température en Fahrenheit : ");
+   scanf("%f",&F);
+
+   C = (F-32)/1.8 ;
+
+   if ( C<=0 ) {
+    printf("%d : tres froid ",C);
+   }
+   else if ( C>=0 && C<=15 ){
+    printf("%d : froid",C);
+   }
+   else if ( C>=15 && C<=30){
+    printf("%d : chaud",C);
+   }
+   else {
+    printf("%d : tres chaud",C);
+   }
+   return 0 ;
+}
