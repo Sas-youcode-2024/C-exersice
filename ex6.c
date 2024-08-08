b35e33173b30a53ab59c7e36b3314513ff637743
ZAKARIA-MOUFID <zakariamoufid766@gmail.com>
2024-08-08 08:18:28 +0100

    exercise 6

---
+++ ex6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+float circo ,rayon ;
+const float p = 3.14 ;
+  printf("entrez le rayon hu cercle");
+  scanf("%f",&rayon);
+
+  circo = 2 * rayon * p ;
+
+  printf("la circonference du cercl est: %.2f",circo)   ;
+
+}
