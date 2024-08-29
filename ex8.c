d61b2c18fc448779d745ba160ec53757ffb970e5
zakaria <zakariamoufid766@gmail.com>
2024-08-29 17:43:41 +0100

    exercise 8

---
+++ ex8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main() {
+
+ int nomber ;
+
+ printf("veuillez saisir un nomber : ");
+ scanf("%d",&nomber);
+
+ printf("hexadecimal est %X \n",nomber);
+ printf("octal est %o \n",nomber);
+
+
+ return 0;
+}
